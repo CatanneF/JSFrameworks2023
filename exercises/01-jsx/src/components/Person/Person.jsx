61cc4d142311a8cc78a435fa2013b136bb5c25ab
Catherine Felton <catherinefelton.design@gmail.com>
2023-06-12 13:42:02 -0400

    Assignment 1 answers

--- exercises/01-jsx/src/components/Person/Person.jsx
+++ exercises/01-jsx/src/components/Person/Person.jsx
@@ -9,17 +9,23 @@ const Person = () => {
    * Create a function that get's Peter Piper's age from his birth year.
    * You can get today's year with:
    * @example new Date(Date.now()).getFullYear()
+   * 
    */
+
+  const getAge = (birthYear) => {
+    const year = new Date(Date.now()).getFullYear();
+    return year - birthYear;
+  };
 
   return (
     <div>
       <h1>Featured Person</h1>
       {/* Using JSX, plugin name below from the person object */}
-      <div>Name: _____</div>
+      <div>Name: {person.name}</div>
       {/* Using JSX, plugin occupation below from the person object */}
-      <div>Occupation: _____</div>
+      <div>Occupation: {person.occupation}</div>
       {/* Using JSX, plugin the birth year using a function that converts age to birth year */}
-      <div>Age: _____</div>
+      <div>Age: {getAge(person.birthYear)}</div>
     </div>
   );
 };
