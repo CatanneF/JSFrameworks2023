b3648280e40a424ed2fabb7abf85d0c85e03c3b2
Catherine Felton <catherinefelton.design@gmail.com>
2023-06-13 20:04:27 -0400

    assignment 3

--- exercises/03-props/src/App.jsx
+++ exercises/03-props/src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import Welcome from "./components/Welcome/Welcome"
+import Heading from "./components/Heading/Heading"
 // import something here
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return (
+    <div className="App">
+    <Welcome name="Catherine" />
+    <Heading>Welcome to my React app!</Heading>
+    </div>
+  )
 }
+
+
+
+
 
 export default App;
---
+++ exercises/03-props/src/components/Welcome/Welcome.jsx
@@ -0,0 +1,4 @@
+const Welcome = ({name = "user"}) => {
+    return  <h1>Hello {name}</h1>
+}
+export default Welcome;
