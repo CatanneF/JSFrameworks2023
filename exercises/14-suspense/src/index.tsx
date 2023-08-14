ce33cea3fd6bb17799c01dfd977749cf14e75e4b
Catherine Felton <catherinefelton.design@gmail.com>
2023-08-14 09:52:08 -0400

    assignment 14 and 15 fixes/ answers

---
+++ exercises/14-suspense/src/index.tsx
@@ -0,0 +1,11 @@
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
